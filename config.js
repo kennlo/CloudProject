f1800522586eb6526a97e73641534135eff8c1b4
kennlo <97938823+kennlo@users.noreply.github.com>
2024-03-27 21:23:42 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+window._config = {
+
+    cognito: {
+
+        userPoolId: 'us-east-1_fMesu4GkG', // e.g. us-east-2_uXboG5pAb         
+
+        userPoolClientId: '5mhu24i2ri3ddubqcejonv4itc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        region: 'us-east-1' // e.g. us-east-2 
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://nynaa6l7pk.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
+};

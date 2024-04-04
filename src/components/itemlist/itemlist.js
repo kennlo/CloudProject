de921e5f688646625a4ed86adde8b0f512559674
JamusLau <44183679+JamusLau@users.noreply.github.com>
2024-04-04 20:42:13 +0800

    page split

--- src/components/itemlist/itemlist.js
+++ src/components/itemlist/itemlist.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { Auth } from 'aws-amplify';
 import axios from 'axios';
-import TableDisplay from '../table/TableDisplay';
+// import TableDisplay from '../table/TableDisplay';
 import ItemDisplay from '../itemlist/itemdisplay';
 
 export default class ItemList extends Component {
@@ -77,46 +77,58 @@ export default class ItemList extends Component {
   render() {
     return (
       <Fragment>
-        <section className="section">
-          <div className="container">
-            <h1>Items</h1>
-            <p className="subtitle is-5">All the items available.</p>
-            <TableDisplay tableData={this.state.tableData} />
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div style={{ width: '70%' }}>
+            <section className="section">
+              {/* <div className="container">
+                <h1>Items</h1>
+                <p className="subtitle is-5">All the items available.</p>
+                <TableDisplay tableData={this.state.tableData} />
+              </div> */}
+              <div className="container">
+                <h1>Items</h1>
+                <p className="subtitle is-5">All the items available.</p>
+                <ItemDisplay tableData={this.state.tableData} />
+              </div>
+              <div>
+                {this.state.user === 'cb2550db-00e0-4210-8be7-61853387898c' ? (
+                  <form onSubmit={this.handleSend}>
+                    <label>ImageURL</label>
+                    <input
+                      type="string"
+                      name="imageURL"
+                      onChange={this.handleChange}
+                      value={this.state.imageURL}
+                    />
+                    <label>Name</label>
+                    <input
+                      type="string"
+                      name="name"
+                      onChange={this.handleChange}
+                      value={this.state.name}
+                    />
+                    <label>Price</label>
+                    <input
+                      type="number"
+                      name="price"
+                      onChange={this.handleChange}
+                      value={this.state.price}
+                    />
+                    <button type="submit">Add Item</button>
+                  </form>
+                ) : null}
+              </div>
+            </section>
           </div>
-          <div className="container">
-            <h1>Items</h1>
-            <p className="subtitle is-5">All the items available.</p>
-            <ItemDisplay tableData={this.state.tableData} />
+          <div style={{ width: '30%' }}>
+            <section className="section">
+              <div className='container'>
+                <h1>Cart</h1>
+                
+              </div>
+            </section>
           </div>
-          <div>
-            {this.state.user === 'cb2550db-00e0-4210-8be7-61853387898c' ? (
-              <form onSubmit={this.handleSend}>
-                <label>ImageURL</label>
-                <input
-                  type="string"
-                  name="imageURL"
-                  onChange={this.handleChange}
-                  value={this.state.imageURL}
-                />
-                <label>Name</label>
-                <input
-                  type="string"
-                  name="name"
-                  onChange={this.handleChange}
-                  value={this.state.name}
-                />
-                <label>Price</label>
-                <input
-                  type="number"
-                  name="price"
-                  onChange={this.handleChange}
-                  value={this.state.price}
-                />
-                <button type="submit">Add Item</button>
-              </form> 
-            ) : null }
-          </div>
-        </section>
+        </div>
       </Fragment>
     );
   }
